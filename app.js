9c410d84b4c5a1edd3d23887478f59f21dd6e154
deijaparker <deijaparker1@outlook.com>
2024-07-31 06:59:54 -0700

    Add averageArray function to calculate the average of an array of numbers

--- app.js
+++ app.js
@@ -4,3 +4,9 @@
 function sumArray(numbers) {
     return numbers.reduce((sum, num) => sum + num, 0);
 }
+
+// Function to take an array of numbers and return the average
+function averageArray(numbers) {
+    const total = sumArray(numbers);
+    return total / numbers.length;
+}

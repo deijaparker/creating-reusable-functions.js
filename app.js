a4be0f3b46a45922c7563dc0de9a11a52247fe29
deijaparker <deijaparker1@outlook.com>
2024-07-31 07:08:19 -0700

    Input sortedByAge to sort the array by age

--- app.js
+++ app.js
@@ -30,3 +30,17 @@ function printNumbers(n) {
     }
     recursivePrint(1);
 }
+
+// Part 2: Thinking Methodically
+
+const data = [
+    { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+    { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+    { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+    { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+    { id: "7", name: "Bilbo", occupation: "None", age: "111" }
+];
+
+// Sort the array by age
+const sortedByAge = data.sort((a, b) => parseInt(a.age) - parseInt(b.age));
+console.log(sortedByAge);
